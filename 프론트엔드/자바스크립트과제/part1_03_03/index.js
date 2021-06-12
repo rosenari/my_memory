2d968c40432ae6350cd935b287cae016953556b0
임장순 <rosenari88@gmail.com>
2021-06-13 01:44:39 +0900

    Add / 자바스크립트과제 - 3-3

---
+++ 프론트엔드/자바스크립트과제/part1_03_03/index.js
@@ -0,0 +1,10 @@
+/**
+ * x를 n번 곱한 수를 반환함
+ * 
+ * @param {number} x 거듭제곱할 숫자
+ * @param {number} n 곱할 횟수, n >= 1
+ * @return {number} x의 n 거듭제곱을 반환함
+ */
+function pow(x, n) {
+    return x ** n;
+}
